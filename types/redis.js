fbf7348f3338090e3f0639f4c19ddf03276478bf
Michael Osuna Jr <mike@plasso.co>
2017-07-01 17:18:26 -0700

    fix flow errors in node.js

--- types/redis.js
+++ types/redis.js
@@ -3,11 +3,24 @@ interface RedisErrorCallback {
 }
 
 interface RedisDataCallback {
-    (data?: any, err?: Error): void;
+    (err?: Error, data?: any): void;
 }
+
+interface RedisMultiClient {
+  get(id: string, cb: RedisDataCallback): void;
+  mget(ids: Array<string>, cb: RedisDataCallback): void;
+  set(id: string, data: string, cb: RedisErrorCallback): void;
+  setnx(id: string, data: string, cb: RedisErrorCallback): void;
+  exec(RedisDataCallback): void;
+}
+
 interface RedisClient {
   get(id: string, cb: RedisDataCallback): void;
+  mget(ids: Array<string>, cb: RedisDataCallback): void;
   set(id: string, data: string, cb: RedisErrorCallback): void;
+  setnx(id: string, data: string, cb: RedisErrorCallback): void;
+  watch(id: string): void;
+  multi(): RedisMultiClient;
 }
 
 interface Redis {
