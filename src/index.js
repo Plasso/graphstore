1ab5b9f4d0fa3a88171e67f210cd1afa141f900d
Michael Osuna Jr <mike@plasso.com>
2017-07-28 02:57:30 +0000

    export registries

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import CachedEdge from './cached-edge';
 import CachedNode from './cached-node';
 import MemoryEdge from './memory-edge';
 import MemoryNode from './memory-node';
+import NodeRegistry from './node-regsitry';
+import EdgeRegistry from './edge-regsitry';
 import toConnection from './to-connection';
 
 module.exports = {
@@ -10,5 +12,7 @@ module.exports = {
   MemoryEdge,
   MemoryNode,
   toConnection,
+  NodeRegistry,
+  EdgeRegistry,
 };
 
